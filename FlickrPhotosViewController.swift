d14bc163865a19ea82c773b536e0ea7e1b2d3e21
Jesslly Wong <jellywonka@Jessllys-MacBook-Pro.local>
2016-04-26 20:30:35 -0700

    Initial Checkin

---
+++ FlickrPhotosViewController.swift
@@ -0,0 +1,17 @@
+//
+//  FlickrPhotosViewController.swift
+//  FlickrSearch
+//
+//  Created by Jesslly Wong on 2016-04-26.
+//  Copyright © 2016 Wonka Studios. All rights reserved.
+//
+
+import UIKit
+
+private let reuseIdentifier = "FlickrCell"
+private let sectionInsets = UIEdgeInsets(top: 50.0, left: 20.0, bottom: 50.0, right: 20.0)
+
+class FlickrPhotosViewController: UICollectionViewController {
+ 
+
+}
